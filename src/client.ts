0174d9b749bb68cf8532226399cc8837bcb30289
Nicholas Dudfield <ndudfield@gmail.com>
2023-05-08 18:17:50 +0700

    chore: run prettier

--- src/client.ts
+++ src/client.ts
@@ -84,10 +84,10 @@ export class VOPRFClient extends baseClient {
         }
 
         if (
-            !await evaluation.proof.verify_batch(
+            !(await evaluation.proof.verify_batch(
                 [this.gg.generator(), pkS],
                 zip(finData.evalReq.blinded, evaluation.evaluated)
-            )
+            ))
         ) {
             throw new Error('proof failed')
         }
@@ -127,10 +127,10 @@ export class POPRFClient extends baseClient {
         }
 
         if (
-            !await evaluation.proof.verify_batch(
+            !(await evaluation.proof.verify_batch(
                 [this.gg.generator(), tw],
                 zip(evaluation.evaluated, finData.evalReq.blinded)
-            )
+            ))
         ) {
             throw new Error('proof failed')
         }
