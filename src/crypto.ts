91fde8f9e0c890edef22d71b7e3f9e1f97d8abfa
Nicholas Dudfield <ndudfield@gmail.com>
2023-10-07 15:28:04 +0700

    docs: add comment for CryptoImpl/Crypto intermediary roles

--- src/crypto.ts
+++ src/crypto.ts
@@ -1,6 +1,30 @@
 import type { CryptoProvider, HashID } from './cryptoTypes.js'
 import { CryptoSjcl } from './cryptoSjcl.js'
 
+export const DEFAULT_CRYPTO_PROVIDER = CryptoSjcl
+
+/**
+ * The `CryptoImpl` class serves as an intermediary for utilizing a cryptographic provider.
+ * It implements the `CryptoProvider` interface, encapsulating a `CryptoProvider` instance
+ * which can be overridden via the `provider` field. This design allows for flexible
+ * substitution of cryptographic providers while ensuring adherence to the `CryptoProvider`
+ * interface contract.
+ *
+ * The `Crypto` object, instantiated from `CryptoImpl` with a default provider, acts as the
+ * accessible point for cryptographic operations within the library. Users can override the
+ * default cryptographic provider by setting a different provider to the `Crypto.provider` field.
+ *
+ * Usage:
+ * ```javascript
+ * import { Crypto } from './path-to-this-file';
+ * import { YourCryptoProvider } from './your-crypto-provider-file';
+ *
+ * // Override the default crypto provider
+ * Crypto.provider = YourCryptoProvider;
+ *
+ * // Now Crypto will use YourCryptoProvider for cryptographic operations
+ * ```
+ */
 class CryptoImpl implements CryptoProvider {
     constructor(public provider: CryptoProvider) {}
 
@@ -17,5 +41,4 @@ class CryptoImpl implements CryptoProvider {
     }
 }
 
-// There's already a Crypto global which makes auto imports fail
-export const Crypto = new CryptoImpl(CryptoSjcl)
+export const Crypto = new CryptoImpl(DEFAULT_CRYPTO_PROVIDER)
